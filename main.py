f475a0224f2ad1fd9ac44032ceab22cfb36b5d3e
kellygilmore <kellygilmore@dpengineering.org>
2019-10-22 13:32:05 -0700

    Made change direction button work

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
+import spidev
 
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 from pidev.MixPanel import MixPanel
 from pidev.kivy.PassCodeScreen import PassCodeScreen
 from pidev.kivy.PauseScreen import PauseScreen
+from pidev.stepper import stepper
 from pidev.kivy import DPEAButton
 from pidev.kivy import ImageButton
+
+import RPi.GPIO as GPIO
+
+spi = spidev.SpiDev()
+
+s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
+                 steps_per_unit=200, speed=8)
 
 MIXPANEL_TOKEN = "x"
 MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
@@ -23,6 +34,7 @@ class ProjectNameGUI(App):
     """
     Class to handle running the GUI Application
     """
+
 
     def build(self):
         """
@@ -39,126 +51,43 @@ class MainScreen(Screen):
     """
     Class to handle the main screen and its associated touch events
     """
+    motor = ObjectProperty()
+
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.count = 0
-        self.label_text = str(self.count)
+        self.motor = False
+        self.direction = 0
 
-    def increment(self, *args):
-        self.count += 1
-        self.label_text = str(self.count)
-        print(self.label_text)
+    def run_motor(self):
+        self.motor = not self.motor
+        if self.motor:
+            s0.run(0, 20)
+        else:
+            s0.softStop()
 
-    def pressed(self):
-        """
-        Function called on button touch event for button with id: testButton
-        :return: None
-        """
-        PauseScreen.pause(pause_scene_name='pauseScene', transition_back_scene='main', text="Test", pause_duration=5)
-
-    def admin_action(self):
-        """
-        Hidden admin button touch event. Transitions to passCodeScreen.
-        This method is called from pidev/kivy/PassCodeScreen.kv
-        :return: None
-        """
-        SCREEN_MANAGER.current = 'passCode'
+    def change_direction(self):
+        self.direction = not self.direction
+        if self.direction:
+            self.direction = self.direction + 1
+            s0.run(1, 20)
+        else:
+            s0.run(0, 20)
 
 
-class AdminScreen(Screen):
-    """
-    Class to handle the AdminScreen and its functionality
-    """
 
-    def __init__(self, **kwargs):
-
-
-        """
-        Load the AdminScreen.kv file. Set the necessary names of the screens for the PassCodeScreen to transition to.
-        Lastly super Screen's __init__
-        :param kwargs: Normal kivy.uix.screenmanager.Screen attributes
-        """
-        Builder.load_file('AdminScreen.kv')
-
-        PassCodeScreen.set_admin_events_screen(ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
-        PassCodeScreen.set_transition_back_screen(MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
-
-        super(AdminScreen, self).__init__(**kwargs)
-
-    @staticmethod
-    def transition_back():
-        """
-        Transition back to the main screen
-        :return:
-        """
-        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
-
-    @staticmethod
-    def shutdown():
-        """
-        Shutdown the system. This should free all steppers and do any cleanup necessary
-        :return: None
-        """
+    def shutdown(self):
+        s0.free_all()
+        spi.close()
+        GPIO.cleanup()
         os.system("sudo shutdown now")
 
-    @staticmethod
-    def exit_program():
-        """
-        Quit the program. This should free all steppers and do any cleanup necessary
-        :return: None
-        """
-        quit()
-"""
-Widget additions
-"""
-
-Builder.load_file('main.kv')
-SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
-SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
-SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
-SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
-
-"""import os
-
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-from pidev.MixPanel import MixPanel
-from pidev.kivy.PassCodeScreen import PassCodeScreen
-from pidev.kivy.PauseScreen import PauseScreen
-from pidev.kivy import DPEAButton
-from pidev.kivy import ImageButton
-
-MIXPANEL_TOKEN = "x"
-MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
-
-SCREEN_MANAGER = ScreenManager()
-MAIN_SCREEN_NAME = 'main'
-ADMIN_SCREEN_NAME = 'admin'
 
 
-class ProjectNameGUI(App):
-    """
-    Class to handle running the GUI Application
-    """
-
-    def build(self):
-        """
-        Build the application
-        :return: Kivy Screen Manager instance
-        """
-        return SCREEN_MANAGER
 
 
-Window.clearcolor = (1, 1, 1, 1)  # White
 
 
-class MainScreen(Screen):
-    """
-    Class to handle the main screen and its associated touch events
-    """
+
 
     def pressed(self):
         """
@@ -250,7 +179,7 @@ if __name__ == "__main__":
     ProjectNameGUI().run()
 
 MixPanel
-"""
+
 
 
 def send_event(event_name):
